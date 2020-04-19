795dc278990f87a6f1984ea6215189394c0e09ce
irfannurahman08 <irfannurahman082gmail.com>
2020-04-19 18:17:47 +0700

    done

---
+++ WebSite1/App_Code/Service.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+
+public class Service : System.Web.Services.WebService
+{
+    public Service () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public double CelciusToReamur(int C)
+    {
+        return (0.8) * C;
+    }
+    [WebMethod]
+    public double CelciusToFahrenhet(int C)
+    {
+        return (1.8) * C + 32;
+    }
+    [WebMethod]
+    public double CelciusToKelvin(int C)
+    {
+        return C + 273;
+    }
+    
+}
